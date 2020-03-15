3b92323c9de06efefda32447bc90493b7fbddff8
Hamid <hamid.hessamei@gmail.com>
2020-03-16 00:12:18 +0330

    Updates

---
+++ precache-manifest.dd7f03186d9f5a461fb3cc33e81d104a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d18afaa621c1249b8bad6207cde6bf68",
+    "url": "/API/index.html"
+  },
+  {
+    "revision": "f8a5cd8952e32f0d580f",
+    "url": "/API/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "29ab7342ac5467247fd1",
+    "url": "/API/static/js/2.8a91a860.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/API/static/js/2.8a91a860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a5cd8952e32f0d580f",
+    "url": "/API/static/js/main.f53f89d7.chunk.js"
+  },
+  {
+    "revision": "e9360a5751d0e1878588",
+    "url": "/API/static/js/runtime-main.9b4cab74.js"
+  }
+]);
