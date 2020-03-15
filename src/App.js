7041f7ffb3179b59a4e88c793fb83d9cda43f7a1
Hamid <hamid.hessamei@gmail.com>
2020-03-16 00:04:35 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Aval extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: { name: "", family: "", DB: "" }
+    };
+  }
+
+  state = {
+    Entry: ""
+  };
+
+  ChangeInput = event => {
+    this.setState({
+      Entry: event.target.value
+    });
+  };
+
+  fetchUsers = (n=1) => {
+    fetch(`http://edalatkhane.org/api.php?id=${n}`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          user: data
+        })
+      );
+  };
+  Create = event => {
+    if ((event.keyCode == 13) & (this.state.Entry != "")) {
+      this.fetchUsers(this.state.Entry)
+      this.setState({
+        Entry: ""
+      });
+    } else {
+      return;
+    }
+  };
+  // componentDidMount() {
+  //   this.fetchUsers();
+  // }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.Entry}
+          onChange={this.ChangeInput}
+          onKeyDown={this.Create}
+          type="text"
+          className="new-todo"
+          placeholder="id?"
+        />
+        {/* <button onClick={}>ثبت</button> */}
+        <h1>{this.state.user.name}</h1>
+        <h1>{this.state.user.family}</h1>
+        <h1>{this.state.user.BD}</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Aval;
